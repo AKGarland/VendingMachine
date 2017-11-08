973b23309ce144f229f3d16c26609cc6fcf1f0cd
AKGarland <amberkgarland@gmail.com>
2017-11-08 14:15:07 +0000

    Money test etc

---
+++ src/com/company/Money.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Money {
+    public int tenPence = 10;
+    private int twentyPence = 20;
+    private int fiftyPence = 50;
+    private int onePound = 100;
+    private int twoPound = 200;
+
+    public int getTenPence() {
+        return tenPence;
+    }
+
+    public int getTwentyPence() {
+        return twentyPence;
+    }
+
+    public int getFiftyPence() {
+        return fiftyPence;
+    }
+
+    public int getOnePound() {
+        return onePound;
+    }
+
+    public int getTwoPound() {
+        return twoPound;
+    }
+}
--- src/com/company/MoneyTest.java
+++ src/com/company/MoneyTest.java
@@ -1,10 +1,42 @@
 package com.company;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
 public class MoneyTest {
+    private Money money;
+
+    @Before
+    public void setUp() {
+        money = new Money();
+    }
     @Test
     public void moneyClassHasTenPence(){
-        
+        assertEquals(10,money.getTenPence());
+    }
+
+    @Test
+    public void moneyClassHasTwentyPence(){
+        assertEquals(20,money.getTwentyPence());
+    }
+
+    @Test
+    public void moneyClassHasFiftyPence(){
+        assertEquals(50,money.getFiftyPence());
+    }
+
+    @Test
+    public void moneyClassHasOnePound(){
+        assertEquals(100,money.getOnePound());
+    }
+
+    @Test
+    public void moneyClassHasTwoPound(){
+        assertEquals(200,money.getTwoPound());
     }
 }
